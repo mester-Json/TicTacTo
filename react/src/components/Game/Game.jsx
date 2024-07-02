c3da2da40bb26578ca2c189894478f2ff0de884b
mester-Json <decubberjayson@gmail.com>
2024-07-02 13:24:18 +0200

    Ajout

--- react/src/components/Game/Game.jsx
+++ react/src/components/Game/Game.jsx
@@ -6,7 +6,7 @@ function Game() {
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState(0);
     const xIsNext = currentMove % 2 === 0;
-    const currentSquares = history[currentMove] || Array(9).fill(null);  // Ensure currentSquares is always an array with 9 elements
+    const currentSquares = history[currentMove] || Array(9).fill(null);
 
     function handlePlay(nextSquares) {
         const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
--- react/src/components/ticttacto/TicTacTo.jsx
+++ react/src/components/ticttacto/TicTacTo.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 
 function Square({ value, onSquareClik }) {
